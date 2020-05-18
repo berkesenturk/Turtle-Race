741295008d4d6be3d6e9b3353cd18c62a11493c7
berkesenturk <45045846+berkesenturk@users.noreply.github.com>
2020-05-18 23:34:32 +0300

    Update turtlerace.py

--- turtlerace.py
+++ turtlerace.py
@@ -1,7 +1,4 @@
 
-
-# kimin kazandığını kontrol etcek
-
 from random  import randint
 from turtle import *
 
@@ -26,7 +23,7 @@ for step in range(25):
     backward(160)
     left(90)
     forward(20) 
-
+#1st Turtle
 a = Turtle(shape='turtle')
 a.color('red')
 
@@ -37,9 +34,7 @@ a.pendown()
 for turn in range(72):
   a.left(5)
 
-
-
-
+#2nd Turtle
 b = Turtle('turtle')
 b.color('green')
 b.penup()
@@ -50,7 +45,7 @@ for turn in range(72):
 
 
 
-
+#3rd Turtle
 c = Turtle('turtle')
 
 c.color('cyan')
@@ -61,7 +56,7 @@ for turn in range(72):
   c.left(5)
 
 
-
+#4th Turtle
 d = Turtle('turtle')
 
 d.color('magenta')
@@ -72,7 +67,7 @@ for turn in range(72):
   d.left(5)
 
 
-
+#5th Turtle
 e = Turtle('turtle')
 
 e.color('brown')
@@ -84,10 +79,7 @@ for turn in range(72):
 
 
 
-
-### Subsequent variations start here ###
-
-
+#Moving func
 def move():
     for turn in range(151):
         a.forward(randint(1,5))
@@ -96,7 +88,6 @@ def move():
         d.forward(randint(1,5))
         e.forward(randint(1,5))
         
-        #bitiren dönsün ordan hesapla.
 screen.onkeypress(move,"space")
 screen.listen()
 
@@ -113,37 +104,9 @@ screen.listen()
 
 
 
-### Subsequent variations end here ###
+
 
 screen.mainloop()
 
 
 
-
-# TURTLE_SIZE = 20
-
-# screen = Screen()
-
-# t1 = Turtle(shape="turtle", visible=False)
-# t1.penup()
-# t1.goto(TURTLE_SIZE/2 - screen.window_width()/2, screen.window_height()/2 - TURTLE_SIZE/2)
-# t1.pendown()
-# t1.showturtle()
-
-
-# t2 = Turtle(shape="turtle", visible=False)
-# t2.penup()
-# t2.goto(TURTLE_SIZE/2 - screen.window_width()/2, screen.window_height()/3 - TURTLE_SIZE/3)
-# t2.pendown()
-# t2.showturtle()
-
-# t3 = Turtle(shape="turtle", visible=False)
-# t3.penup()
-# t3.goto(TURTLE_SIZE/2 - screen.window_width()/2,  3* TURTLE_SIZE)
-# t3.pendown()
-# t3.showturtle()
-
-# t1.forward(screen.window_width())
-# t2.forward
-
-# screen.mainloop()
